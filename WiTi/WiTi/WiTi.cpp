8d5e95251397149219122288b1b1fabb93fff794
maciekpop <maciejpopieluch@gmail.com>
2020-05-06 18:37:18 +0200

    WiTi bez permutacji

--- WiTi/WiTi/WiTi.cpp
+++ WiTi/WiTi/WiTi.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -10,13 +12,14 @@ int n;
 
 struct Task 
 {
-	int p{ 0 }, w{ 0 }, d{ 0 };
+	int id{ 0 }, p{ 0 }, w{ 0 }, d{ 0 };
 };
 
 struct Set 
 {
 	vector<Task> tasksInSet;
 	int P{ 0 };
+	int OPT{ 0 };
 };
 
 const vector<Task> loadFromFile(string fileName)
@@ -30,6 +33,7 @@ const vector<Task> loadFromFile(string fileName)
 		int i = 1; // pozycja 0 nie jest brana pod uwagę, żeby ułatwić identyfikację zadań
 		while (!plik.eof())
 		{
+			T[i].id = i;
 			plik >> T[i].p >> T[i].w >> T[i].d;
 			i++;
 		}
@@ -90,20 +94,44 @@ vector<Set> defineSets(vector<Task> tasks)
 	return sets; // zwracane zbiory
 }
 
-void WiTi(vector<Task> tasks)
+int setWithout_z(int setNumber, int taskNumber) 
 {
+	return setNumber & ~(1 << taskNumber - 1); 
+}
 
-
-
+int WiTi(vector<Set>& sets)
+{
+	int min;
+	int ZSize;
+	for (int i = 1; i < pow(2, n); i++)
+	{
+		Set& Z = sets[i]; // zbiór Z
+		ZSize = Z.tasksInSet.size();
+		min = INT_MAX; 
+		for (auto task : Z.tasksInSet)
+		{
+			Task& z = task; // zadanie z, które będzie odejmowane od zbioru Z
+			Set& Z_no_z = sets[setWithout_z(i, task.id)]; // zbiór Z\{z}
+			Z.OPT = Z_no_z.OPT + z.w * max(0, Z.P - z.d); // wzór (3) z instrukcji
+			if(Z.OPT < min)
+				min = Z.OPT;
+		}
+		Z.OPT = min; // przypisywanie zbiorowi wartości optymalnej
+	}
+	
+	return sets[pow(2, n) - 1].OPT;
 }
 	
+
+
 int main()
 {
-	vector<Set> test;
-	test = defineSets(loadFromFile("dane3.txt"));
-
-	vector<Task> zad = test[15].tasksInSet;
-	for(auto x : zad)
-		cout << x.p <<' ';
-	cout << test[15].P;
+	string name = "dane";
+	string ext = ".txt";
+	for (int i = 1; i <= 9; i++)
+	{
+		string num = to_string(i);
+		vector<Set> sets = defineSets(loadFromFile(name + num + ext));
+		cout << WiTi(sets) << endl;
+	}
 }
